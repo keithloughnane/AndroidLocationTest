b4f16f081de1676aa0baa04459d6ccdead4b4e56
Keith Loughnane <keith.loughnane@gmail.com>
2016-10-11 12:50:28 +0300

    Connection, JSON and Putting pins on the maps are working.

--- AndroidMapsApp/app/src/main/java/com/keithloughnane/androidmapsapp/MapsActivity.java
+++ AndroidMapsApp/app/src/main/java/com/keithloughnane/androidmapsapp/MapsActivity.java
@@ -10,6 +10,7 @@ import android.os.StrictMode;
 
 
 
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -54,44 +55,71 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onClick(View view) {
                 Log.d(TAG,"onClick: btnGetForDates");
 
-                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                StrictMode.setThreadPolicy(policy);
+                getLocations(1457604000);
+            }
+        });
+    }
+
+    public JSONArray getLocations(int unixTime)
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        String response = "";
+
+        JSONArray jobj = new JSONArray();
 
 
+        String str = "http://assignment.autlo.com/coordinates?time=1457604000";
+        try {
+            URL url = new URL(str);
 
-                String str = "http://assignment.autlo.com/coordinates?time=1457604000";
-                try {
-                    URL url = new URL(str);
-                    URLConnection urlc = url.openConnection();
-                    BufferedReader br = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
-                    String line;
-                    while((line = br.readLine())!=null)
-                    {
-                        JSONArray jsn = new JSONArray(line);
-                        for (int i=0; i < jsn.length();i++)
-                        {
+            URLConnection urlc = url.openConnection();
+            BufferedReader br = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
+            String line;
+            //JsonParser parser = new JsonParser();
 
-                            Log.d(TAG, "JSON is " + jsn.get(i).toString());
+            //JSONArray jsn = new JSONArray(line);
+            while((line = br.readLine())!=null)
+            {
 
-                            JSONObject jo = (JSONObject)jsn.get(i);
+                response = response + line; //This can be more better
 
+            }
+            Log.d(TAG,response);
 
+        }
+        catch (Exception e)
+        {
+            Log.d(TAG, "onClick Net: " + e.toString());
+            e.printStackTrace();
+        }
 
+        try {
+            jobj  = new JSONArray(response);
+        }
+        catch (Exception e)
+        {
+            Log.d(TAG, e.toString());
+        }
+        if(jobj != null) {
 
+           // Log.d(TAG, "getLocations: Returning JSONArray " + jobj.get(0));
 
-                        }
+            try {
 
+                    for (int i = 0; i < jobj.length(); i++) {
+                        Log.d(TAG, "getLocations: Returning JSONArray " + jobj.getJSONObject(i).get("date_created"));
+
+                        mMap.addMarker(new MarkerOptions().position(new LatLng(jobj.getJSONObject(i).getDouble("lat"),jobj.getJSONObject(i).getDouble("lng"))));
 
                     }
                 }
-                catch (Exception e)
+                catch(Exception e)
                 {
-                    Log.d(TAG, "onClick Net: " + e.toString());
                     e.printStackTrace();
                 }
-
-            }
-        });
+        }
+        return jobj;
     }
 
 
@@ -108,10 +136,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        /*
+        Latitude	59.436962
+        Longitude	24.753574
+                */
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng tallinn = new LatLng(59.436962, 24.753574);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(tallinn));
+        mMap.setMinZoomPreference(14);
     }
 
 
